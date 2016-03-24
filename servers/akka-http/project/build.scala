d23f6d23995be01eb4dd89d4a99648f27ce30d84
Andrii Stasiuk <andrii.stasiuk@playtech.com>
2016-03-24 19:44:08 +0200

    add akka-http

---
+++ servers/akka-http/project/build.scala
@@ -0,0 +1,31 @@
+package wsbench
+
+import sbt._,Keys._
+
+object Build extends Build {
+
+	override lazy val settings = super.settings ++ Seq(
+    name := "wsbench-akka-http",
+    organization := "com.github.wsbench",
+    version := "0.0.1-SNAPSHOT",
+    scalaVersion := "2.11.8",
+    scalacOptions ++= Seq(
+      "-feature",
+      "-unchecked",
+      "-deprecation"
+    )
+  )
+
+	lazy val root = Project(id="root",base=file(".")).settings(
+    libraryDependencies ++= Seq(
+    	"com.typesafe.akka" %% "akka-http-core" % "2.4.2"
+    ),
+    mainClass in (Compile, run) := Some(""),
+    fork in run := true,
+    javaOptions in run ++= Seq(
+      "-server",
+      "-Xms512m",
+      "-Xmx512m"
+    )
+  )
+}
